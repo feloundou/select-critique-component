63ab736c05ef74e16d0ea022d3398d65f7ba4024
Tyna <mfe25@cornell.edu>
2023-06-29 19:05:23 -0700

    hacky attempt at streaming responses

--- my_component/__init__.py
+++ my_component/__init__.py
@@ -1,6 +1,6 @@
 import os
 import streamlit.components.v1 as components
-
+import openai
 
 # Create a _RELEASE constant. We'll set this to False while we're developing
 # the component, and True when we're ready to package and distribute it.
@@ -18,46 +18,181 @@ else:
     build_dir = os.path.join(parent_dir, "frontend/build")
     _component_func = components.declare_component("my_component", path=build_dir)
 
+# def my_component(name, ai_response="", key=None):
+#     component_value = _component_func(name=name, key=key, default={})
+#     return component_value
 
-def my_component(name, key=None):
+# def my_component(prompt=None, ai_response=None, key=None):
+#     component_value = _component_func(ai_response=ai_response, key=key, default={})
+#     return component_value
 
-    component_value = _component_func(name=name, key=key, default=0)
-
-    # We could modify the value returned from the component if we wanted.
-    # There's no need to do this in our simple example - but it's an option.
+def my_component(critique_clicked=None, ai_response=None, key=None):
+    component_value = _component_func(
+        critique_clicked=critique_clicked, ai_response=ai_response, key=key, default={}
+    )
     return component_value
 
 
+##################################################################################
+
+import os
+import streamlit.components.v1 as components
 import streamlit as st
+import openai
+
+def my_component(critique_clicked=None, ai_response=None, key=None):
+    component_value = _component_func(
+        critique_clicked=critique_clicked, ai_response=ai_response, key=key, default={}
+    )
+    return component_value
 
 st.set_page_config(layout="wide")
 
-my_component("World")
+if "model_response_str" not in st.session_state:
+    st.session_state.model_response_str = ""
+if "critique_clicked" not in st.session_state:
+    st.session_state.critique_clicked = False
 
-# # Add some test code to play with the component while it's in development.
-# # During development, we can run this just as we would any other Streamlit
-# # app: `$ streamlit run my_component/__init__.py`
-# if not _RELEASE:
-#     import streamlit as st
+# response = my_component(
+#     critique_clicked=st.session_state.critique_clicked,
+#     ai_response=st.session_state.model_response_str,
+# )
 
-#     st.subheader("Component with constant args")
+# response = my_component(ai_response=st.session_state.get("model_response_str", ""), key=st.session_state.get("unique_key", 0))
 
-#     # Create an instance of our component with a constant `name` arg, and
-#     # print its output value.
-#     num_clicks = my_component("World")
-#     st.markdown("You've clicked %s times!" % int(num_clicks))
+response = my_component(
+    critique_clicked=st.session_state.get("critique_clicked", False),
+    ai_response=st.session_state.get("model_response_str", ""),
+    key=st.session_state.get("unique_key", 0)
+)
 
-#     st.markdown("---")
-#     st.subheader("Component with variable args")
+if response and response.get("critique_clicked", False):
+    critique = response["critique"]
+    print("critique clicked")
+    print(critique)
 
-#     # Create a second instance of our component whose `name` arg will vary
-#     # based on a text_input widget.
-#     #
-#     # We use the special "key" argument to assign a fixed identity to this
-#     # component instance. By default, when a component's arguments change,
-#     # it is considered a new instance and will be re-mounted on the frontend
-#     # and lose its current state. In this case, we want to vary the component's
-#     # "name" argument without having it get recreated.
-#     name_input = st.text_input("Enter a name", value="Streamlit")
-#     num_clicks = my_component(name_input, key="foo")
-#     st.markdown("You've clicked %s times!" % int(num_clicks))
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    model = "gpt-3.5-turbo"
+    temperature = 1
+
+    prompt = "Why is the sky blue?"
+    steered_system_message = "You are helping answer questions by a user"
+
+    model_response = openai.ChatCompletion.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": steered_system_message},
+            {"role": "user", "content": prompt},
+        ],
+        temperature=temperature,
+    )
+    print("Model response:")
+    print(model_response)
+
+    st.session_state.model_response_str = model_response["choices"][0]["message"]["content"]
+    st.session_state.critique_clicked = False
+    st.session_state.unique_key = st.session_state.get("unique_key", 0) + 1
+
+
+
+# import os
+# import streamlit.components.v1 as components
+# import streamlit as st
+# import openai
+
+# def my_component(ai_response=None, key=None):
+#     component_value = _component_func(ai_response=ai_response, key=key, default={})
+#     return component_value
+
+# def call_model(prompt):
+#     openai.api_key = os.getenv("OPENAI_API_KEY")
+#     model = "gpt-3.5-turbo"
+#     temperature = 1
+#     steered_system_message = "You are helping answer questions by a user"
+
+#     model_response = openai.ChatCompletion.create(
+#         model=model,
+#         messages=[
+#             {"role": "system", "content": steered_system_message},
+#             {"role": "user", "content": prompt},
+#         ],
+#         temperature=temperature,
+#     )
+#     print("Model response:")
+#     print(model_response)
+#     return model_response["choices"][0]["message"]["content"]
+
+# st.set_page_config(layout="wide")
+
+# if "model_response_str" not in st.session_state:
+#     st.session_state.model_response_str = ""
+
+# response = my_component(ai_response=st.session_state.model_response_str)
+
+# if response and response.get("critique_clicked", False):
+#     level = response["level"]
+#     index = response["index"]
+#     critique = response["critique"]
+#     print("critique clicked")
+#     print(critique)
+
+#     model_response_str = call_model(critique)
+#     print("Model response:")
+#     print(model_response_str)
+
+#     st.session_state.model_response_str = model_response_str
+
+
+# ##################################################################################
+# import streamlit as st
+# import openai
+
+# st.set_page_config(layout="wide")
+
+# # Store the response from my_component in a variable
+# # response = my_component("World")
+# # print(attr(my_component))
+
+# # model_response_str = ""
+
+# # response = my_component(ai_response=model_response_str)
+
+
+# if "model_response_str" not in st.session_state:
+#     st.session_state.model_response_str = ""
+
+# response = my_component(ai_response=st.session_state.model_response_str)
+
+# if response and response.get("critique_clicked", False):
+#     level = response["level"]
+#     index = response["index"]
+#     critique = response["critique"]
+#     print("critique clicked")
+#     print(critique)
+
+#     openai.api_key = os.getenv("OPENAI_API_KEY")
+
+#     model = "gpt-3.5-turbo"
+#     temperature = 1
+
+#     prompt = "Why is the sky blue?"
+#     steered_system_message = "You are helping answer questions by a user"
+
+#     model_response = openai.ChatCompletion.create(
+#         model=model,
+#         messages=[
+#             {"role": "system", "content": steered_system_message},
+#             {"role": "user", "content": prompt},
+#         ],
+#         temperature=temperature,
+#     )
+#     print("Model response:")
+#     print(model_response)
+
+#     # model_response_str = model_response["choices"][0]["message"]["content"]
+#     st.session_state.model_response_str = model_response["choices"][0]["message"]["content"]
+
+#     # print(model_response_str)
+#     st.experimental_rerun()
+
